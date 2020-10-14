fc5902cd61c08c2afef225b194a80193b6f5e653
Theron7 <72846686+Theron7@users.noreply.github.com>
2020-10-14 17:40:15 -0400

    Updated names

--- books/settings.py
+++ books/settings.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-BOT_NAME = 'books'
+BOT_NAME = 'Mepra'
 
-SPIDER_MODULES = ['books.spiders']
-NEWSPIDER_MODULE = 'books.spiders'
+SPIDER_MODULES = ['Flatware.spiders']
+NEWSPIDER_MODULE = 'Flatware.spiders'
 
 ROBOTSTXT_OBEY = True
 HTTPCACHE_ENABLED = True
