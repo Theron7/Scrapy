1087220d6f2195930ae90be837b989e73f8fac02
Theron7 <72846686+Theron7@users.noreply.github.com>
2020-10-14 12:47:03 -0400

    Commented Item back in

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -25,6 +25,6 @@ class BooksSpider(scrapy.Spider):
         item = {}
         product = response.css("div.product-shop")
         item["title"] = product.css(".product-name h1 ::text").extract_first()
-        #item["number"] = product.css(".product-name span.sku ::text").extract_first()
+        item["number"] = product.css(".product-name span.sku ::text").extract_first()
         #item['price'] = product.css(".price-info .price-box span.price ::text").extract_first()
         yield item
